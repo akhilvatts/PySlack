46e48f288967257322ba81ffe9bac26a9bca61fc
Akhil Sharma <akhilvatts@users.noreply.github.com>
2020-03-27 22:15:18 +0530

    Add error handling

--- PySlack-SendMessageWebhook.py
+++ PySlack-SendMessageWebhook.py
@@ -9,4 +9,9 @@ data = {
    'icon_emoji': ':robot_face:'
 }
 
-requests.post(wekbook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+response = requests.post(wekbook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
+if response.status_code != 200:
+        raise ValueError(
+            'Request to slack returned an error %s, the response is:\n%s'
+            % (response.status_code, response.text)
+        )
